3e70152179a844e0211b585e172c584271811c4a
JvSobral <131224860+JvSobral@users.noreply.github.com>
2023-05-13 00:45:18 -0300

    Add files via upload

---
+++ src/Compra.java
@@ -0,0 +1,23 @@
+public class Compra {
+    private String descricao;
+    private double valor;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public Compra(String descricao, double valor) {
+        this.descricao = descricao;
+        this.valor = valor;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra:" + "descricao= " + descricao  +
+                " valor =" + valor;
+    }
+}
